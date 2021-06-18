2743ab19418113a8fce6863c3c0d6f61e8a70197
Ap2626 <adpar2001@gmail.com>
2021-06-17 22:32:17 -0400

    basic routing and opening screen completed

--- solace_ui/lib/main.dart
+++ solace_ui/lib/main.dart
@@ -8,13 +8,13 @@ import 'redux/app_state.dart';
 import 'redux/reducers/reducers.dart';
 import 'redux/sagas/sagas.dart';
 
+import 'opening_screen.dart';
+
 void main() {
   var sagaMiddleware = createSagaMiddleware();
   final store = Store<AppState>(
     appReducer,
-    initialState: AppState(
-      placeholder: 0
-    ),
+    initialState: AppState(placeholder: 0),
     middleware: [applyMiddleware(sagaMiddleware)],
   );
 
@@ -27,7 +27,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   final Store<AppState> store;
 
   MyApp({Key key, this.store}) : super(key: key);
@@ -37,56 +36,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
+        elevatedButtonTheme: ElevatedButtonThemeData (
+          style: ElevatedButton.styleFrom(
+            primary: Colors.white,
+            minimumSize: Size(250, 51),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+                side: BorderSide(color: Colors.grey),
+            ),
+          ),
+        ),
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+    return OpeningScreen();
   }
 }
---
+++ solace_ui/lib/opening_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:solace_ui/signup_screen.dart';
+
+import 'login_screen.dart';
+
+class OpeningScreen extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            margin: EdgeInsets.fromLTRB(20, 50, 20, 50),
+
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                    Container (
+                        height: 200,
+                        child: Image(image: AssetImage('lib/assets/slogo.jpg')),
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.3,),
+                    ElevatedButton(
+                        onPressed: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginScreen())
+                        ),
+                        child: Text('Log In'),
+                    ),
+                    SizedBox(height: 20,),
+                    ElevatedButton(
+                        onPressed: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => SignupScreen())
+                        ),
+                        child: Text('New User'),
+                    ),
+                ],
+            ),
+        );
+    }
+}
